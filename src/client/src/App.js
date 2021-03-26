1d6e757656572d75ae4e621993cb11061f8f8b44
Rodrigo Toyama <toyama.rodrigo@gmail.com>
2021-03-26 14:59:00 -0300

    front side, tasks

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,11 +1,157 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Button,
+  Box,
+  Container,
+  Grid,
+  Card,
+  CardContent,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  FormControl,
+  InputLabel,
+  Input,
+  Snackbar,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
 import './App.css';
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
+
+  const [task, setTask] = useState({
+    title: '',
+    description: '',
+  });
+  const [snackBar, setSnackBar] = useState(false);
+  const handleInputChange = (e) => {
+    setTask({ ...task, [e.target.name]: e.target.value });
+  };
+
+  const handleSnackBar = (e) => {
+    setSnackBar(true);
+  };
+
+  const handleCloseSnackbar = (e, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setSnackBar(false);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log(task);
+
+    await fetch('http://localhost:5000/api/tasks', {
+      method: 'POST',
+      body: JSON.stringify(task),
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        handleSnackBar();
+      })
+      .catch((err) => console.log(err));
+
+    console.log('Adding task');
+  };
+
+  useEffect(() => {
+
+    
+
+  }, [input])
+
   return (
-    <div className="App">
-      HELLO WORLD
-    </div>
+    <Box className="App">
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              MERN TODO
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item xs={5}>
+            <Card>
+              <form onSubmit={handleSubmit}>
+                <CardContent>
+                  <FormControl fullWidth={true} required={true}>
+                    <InputLabel htmlFor="title">Title</InputLabel>
+                    <Input
+                      id="title"
+                      name="title"
+                      onChange={handleInputChange}
+                      value={task.title}
+                    />
+                  </FormControl>
+                </CardContent>
+                <CardContent>
+                  <FormControl fullWidth={true} required={true}>
+                    <InputLabel htmlFor="description">Description</InputLabel>
+                    <Input
+                      id="description"
+                      name="description"
+                      onChange={handleInputChange}
+                      value={task.description}
+                    />
+                  </FormControl>
+                </CardContent>
+                <CardContent>
+                  <Button type="submit" variant="contained" color="primary">
+                    Primary
+                  </Button>
+                  <Button variant="contained" color="secondary">
+                    Secondary
+                  </Button>
+                </CardContent>
+              </form>
+            </Card>
+          </Grid>
+          <Grid item xs={7}>
+            texto
+          </Grid>
+        </Grid>
+      </Container>
+      <Snackbar
+        open={snackBar}
+        onClose={handleCloseSnackbar}
+        message="Task added"
+        autoHideDuration={6000}
+      />
+    </Box>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan'); // shows response in terminal
+var cors = require('cors')
 const app = express();
 
 const { mongoose } = require('./database');
@@ -11,6 +12,7 @@ app.set('port', process.env.PORT || 5000); // use OS port or 3000
 // Middlewares (Functions before reaching routes URLS)
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use('/api/tasks', require('./routes/task.routes.js'));
