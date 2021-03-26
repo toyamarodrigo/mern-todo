93fe32fcd82b6368564de362d34f67be87d5837e
Rodrigo Toyama <toyama.rodrigo@gmail.com>
2021-03-26 05:39:20 -0300

    front create-react-app

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express();
 const { mongoose } = require('./database');
 
 // Settings (Config)
-app.set('port', process.env.PORT || 3000); // use OS port or 3000
+app.set('port', process.env.PORT || 5000); // use OS port or 3000
 
 // Middlewares (Functions before reaching routes URLS)
 app.use(morgan('dev'));
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use('/api/tasks', require('./routes/task.routes.js'));
 
 // Static Files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './client/public')));
 
 // Start server
 app.listen(app.get('port'), () => {
